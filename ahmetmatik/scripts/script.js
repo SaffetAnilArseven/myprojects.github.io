8cf137113ae4e4e44f14b55d86ef579fd85e3e60
SaffetAnilArseven <37897271+SaffetAnilArseven@users.noreply.github.com>
2018-06-20 17:35:50 +0300

    Update script.js

--- ahmetmatik/scripts/script.js
+++ ahmetmatik/scripts/script.js
@@ -1,5 +1,6 @@
-﻿if ((self.parent && !(self.parent === self)) && (self.parent.frames.length != 0)) {
+if ((self.parent && !(self.parent === self)) && (self.parent.frames.length != 0)) {
     window.stop();
     document.execCommand('Stop');
     self.parent.location = document.location;
 }
+console.log("[scripts/script.js]")
