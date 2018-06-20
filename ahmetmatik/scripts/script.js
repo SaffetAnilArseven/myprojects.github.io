ce7a32f84f25c8fa1d444ac7940f6c8362a64230
SaffetAnilArseven <37897271+SaffetAnilArseven@users.noreply.github.com>
2018-06-20 17:41:53 +0300

    Delete script.js

--- ahmetmatik/scripts/script.js
+++
@@ -1,6 +0,0 @@
-if ((self.parent && !(self.parent === self)) && (self.parent.frames.length != 0)) {
-    window.stop();
-    document.execCommand('Stop');
-    self.parent.location = document.location;
-}
-console.log("[scripts/script.js]")
